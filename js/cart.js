c4baa0bbbcecac02b5817b9f73aef088bf6f263f
escapingmatr <js.scott.park@gmail.com>
2023-10-21 00:21:52 -0400

    paypal api added

--- js/cart.js
+++ js/cart.js
@@ -50,6 +50,17 @@ function calculateTotalPrice() {
   totalPriceDiv.textContent = `Total Price: $${total.toFixed(2)}`;
 }
 
+function calculateTotalDollar() {
+  const cartItems = loadCartItems();
+  const totalPriceDiv = document.getElementById('total-price');
+
+  const total = cartItems.reduce(
+    (sum, item) => sum + parseFloat(item.price),
+    0
+  );
+  return total;
+}
+
 // Function to remove a cart item
 function removeCartItem(index) {
   const cartItems = loadCartItems();
@@ -57,10 +68,37 @@ function removeCartItem(index) {
   localStorage.setItem('intentions-cart', JSON.stringify(cartItems));
   displayCartItems();
   calculateTotalPrice();
+  updateCartDisplay();
+}
+
+function isCartEmpty() {
+  const cart = JSON.parse(localStorage.getItem('intentions-cart')) || [];
+  return cart.length === 0;
+}
+
+function updateCartDisplay() {
+  const cartItems = document.getElementById('cart-items');
+  const emptyCartMessage = document.getElementById('empty-cart-message');
+  const paypalButtons = document.getElementById('purchase-options');
+
+  if (isCartEmpty()) {
+    cartItems.style.display = 'none';
+    emptyCartMessage.style.display = 'block';
+    paypalButtons.style.display = 'none';
+  } else {
+    cartItems.style.display = 'block';
+    emptyCartMessage.style.display = 'none';
+    paypalButtons.style.display = 'block';
+  }
+}
+
+function startShopping() {
+  window.location.href = '../views/shop.html'; // Redirect to the shop page
 }
 
 // When the page loads, display cart items and total price
 window.addEventListener('load', () => {
   displayCartItems();
   calculateTotalPrice();
+  updateCartDisplay();
 });
